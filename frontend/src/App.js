e0ddf9adc9e50d2c425e4f33f27cac4ab758e015
armantuto <armantuto1992@gmail.com>
2024-01-08 23:43:17 +0100

    aa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Login} />
        <Route component={MainComponent} />  
+       <Redirect to='/' />
          </Switch>
     </>
   );
